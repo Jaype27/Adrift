3a2d573498626017f6dcdae50454a1c95778d8f7
Alex Rochon <alex1rochon23@outlook.com>
2017-11-10 18:18:33 -0500

    began enemy behaviour

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour {
-
-	public Transform target;
-	public Transform m_Transform;
-	public float m_enemyMove;
-	
-	// Update is called once per frame
-	void Update () {
-		transform.LookAt(target);
-		transform.Translate(Vector3.forward * m_enemyMove * Time.deltaTime);
+	public float m_speed;
+	private Rigidbody m_rb;
+	void Start () {
+		m_rb = GetComponent<Rigidbody>();
+		m_rb.velocity = transform.right * m_speed;
 	}
 }
