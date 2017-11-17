c71dda197f918ba8abc973c36bd36e72a132a1ee
Alex Rochon <alex1rochon23@outlook.com>
2017-11-16 21:40:55 -0500

    RaftMove

--- Assets/Scripts/RaftMove.cs
+++ Assets/Scripts/RaftMove.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RaftMove : MonoBehaviour {
 
 	public float m_speed;
 	public GameObject raftSprite;
+	public GameManager m_gm;
 	private Rigidbody m_rb;
 	
 	void Start () {
@@ -18,6 +20,14 @@ public class RaftMove : MonoBehaviour {
 	public IEnumerator Disappear () {
 		yield return new WaitForSeconds(5f);
 		raftSprite.gameObject.SetActive(false);
+		OnBecameInvisible();
 
+	}
+
+	void OnBecameInvisible() {
+		//Scene Main = SceneManager.GetActiveScene ();
+     	SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+		 m_gm.Respawn();
+		
 	}
 }
