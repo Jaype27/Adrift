c146cd9dc0b950cb470ca7237d76418f885e4f01
Jaype27 <jaypee.0927@gmail.com>
2017-11-16 20:53:53 -0500

    raft moves right then disappear

---
+++ Assets/Scripts/RaftMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaftMove : MonoBehaviour {
+
+	public float m_speed;
+	public GameObject raftSprite;
+	private Rigidbody m_rb;
+	
+	void Start () {
+		m_rb = GetComponent<Rigidbody>();
+		m_rb.velocity = transform.right * m_speed;
+
+		StartCoroutine(Disappear());
+	}
+
+	public IEnumerator Disappear () {
+		yield return new WaitForSeconds(5f);
+		raftSprite.gameObject.SetActive(false);
+
+	}
+}
